0a18f582044798632fb6761e51a2a219ec470dc0
Suria <Suria@DESKTOP-9GOKC50>
2019-05-07 02:01:33 +0800

    push

---
+++ WorkshopClubApplication/src/club/Facility.java
@@ -0,0 +1,11 @@
+package club;
+
+public class Facility {
+	private String name;
+	private String description;
+
+	public Facility(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+}
